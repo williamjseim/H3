174bc34f336bf75ceed6d848cbd3f8b008386c3e
williamjseim <33179905+williamjseim@users.noreply.github.com>
2024-02-29 08:47:06 +0100

    sadw

---
+++ Kryptering/symmetrisk kryptering/Kryptering/Encrypters/RsaEncrypter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kryptering.Encrypters
+{
+    internal class RsaEncrypter : EncrypterBase
+    {
+        RSA rsa;
+        public RsaEncrypter()
+        {
+            rsa = RSA.Create();
+            rsa.KeySize = 2048;
+        }
+        public override bool Decrypt(string text, out byte[] EncryptedBytes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Encrypt(string text, out byte[] EncryptedBytes)
+        {
+            EncryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(text), RSAEncryptionPadding.OaepSHA256);
+            return true;
+        }
+
+        public override Tuple<byte[], byte[]> GenerateKeyAndIv()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Kryptering/symmetrisk kryptering/Kryptering/Manager.cs
+++ Kryptering/symmetrisk kryptering/Kryptering/Manager.cs
@@ -24,6 +24,7 @@ namespace Kryptering
             {
                 new AesEncrypter(),
                 new DesEncrypter(),
+                new RsaEncrypter(),
             };
         }
 

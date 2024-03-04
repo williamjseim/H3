2f14151c044a0b69831c8c6cb69e26405654e9d5
williamjseim <williamjseim@gmail.com>
2024-03-04 22:59:19 +0100

    asd

--- pwa/DataGrid/src/app/Components/tabel/tabel.component.ts
+++ pwa/DataGrid/src/app/Components/tabel/tabel.component.ts
@@ -6,25 +6,59 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { ViewChild } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ImageRow } from '../../Interface/image-row';
+import { Observable, merge, of } from 'rxjs';
+
 
 @Component({
   selector: 'app-tabel',
   standalone: true,
-  imports: [MatTable, MatTableModule, MatButtonModule, MatInputModule, MatIconModule, MatTooltipModule, MatMenuModule],
+  imports: [MatTable, MatTableModule, MatButtonModule, MatInputModule, MatIconModule, MatTooltipModule, MatMenuModule, ServiceWorkerModule],
   templateUrl: './tabel.component.html',
   styleUrl: './tabel.component.scss'
 })
 export class TabelComponent {
-  data:ImageData[] = []
+  data$:Observable<ImageRow[]> = new Observable<ImageRow[]>;
+  imageList:ImageRow[] = [];
   displayedColumns: string[] = ['Image', 'DropBox'];
   @ViewChild('UploadImage') UploadButton!:ElementRef<HTMLInputElement>;
+  
+  constructor(){}
 
   ngOnInit(){
+    
+  }
+
+  ngAfterViewInit(){
+    //this.UploadButton.nativeElement.addEventListener("change", this.UploadImage)
+  }
+
+  Test(){
 
   }
   
   UploadImageActivator(){
     this.UploadButton.nativeElement.click();
+  }
+
+  UploadImage(images:File){
+    console.log(this.UploadButton.nativeElement.id)
+    if(this.UploadButton.nativeElement.files!.length >= 1){
+      console.log(this.UploadButton.nativeElement.files?.item(0)?.type);
+    }
+  }
+
+  UploadTest(event:Event){
+    const element = event.currentTarget as HTMLInputElement;
+    if(element.files?.length! > 0){
+      let obj:ImageRow = {} as ImageRow;
+      obj.Image = element.files![0];
+      obj.FileType = element.files![0].type;
+      obj.Size = 50;
+      this.imageList.push(obj);
+      this.data$ = of(this.imageList);
+    }
   }
 
 }
--- pwa/DataGrid/src/app/Interface/image-row.ts
+++ pwa/DataGrid/src/app/Interface/image-row.ts
@@ -2,5 +2,4 @@ export interface ImageRow {
     Image:File;
     Size:number;
     FileType:string;
-    
 }
